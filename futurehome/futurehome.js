cd12834e95014875df25de26191c28dd72b64d18
Arun Poudel <arun@futurehome.no>
2017-09-05 04:20:59 +0545

    Basic websocket connection

--- futurehome/futurehome.js
+++ futurehome/futurehome.js
@@ -2,9 +2,103 @@ module.exports = function(RED) {
     var OAuth2= require('oauth').OAuth2;
     var crypto = require("crypto");
     var request = require('request');
+    var WebSocket = require('ws');
 
     function FuturehomeOAuthNode(config) {
         RED.nodes.createNode(this,config);
+    }
+
+    function FuturehomeSiteNode(config) {
+        RED.nodes.createNode(this,config);
+
+        this.user = config.user || null;
+        this.site  = config.site || null;
+
+        var node = this;
+        var token_valid = false;
+
+        node.status({fill:"yellow",shape:"dot",text:"Not configured"});
+
+        function ifTokenValidConnect() {
+            if (token_valid) {
+                connectWebsocket();
+            } else {
+                setTimeout(ifTokenValidConnect, 5000);
+            }
+        }
+
+        function checkForConfigurationStatus() {
+            if (node.user && node.site) {
+                node.status({fill:"blue",shape:"dot",text:"Checking token validity status."});
+                checkTokenExpiry();
+                setTimeout(ifTokenValidConnect, 500);
+            } else {
+                node.status({fill:"yellow",shape:"dot",text:"Not configured"});
+                var configurationTimeout = setTimeout(checkForConfigurationStatus, 5000);
+            }
+        }
+
+        function checkTokenExpiry() {
+            var credentials = RED.nodes.getCredentials(node.user);
+
+            var api_options = {
+                url: credentials.base_uri + "auth/check",
+                headers: {
+                    'Authorization': 'Bearer ' + credentials.access_token
+                }
+            };
+
+            var r = request.get(api_options,function(err, httpResponse, body) {
+                if(httpResponse.statusCode == 401) {
+                    var oa2 = getOAuth2(credentials.base_uri, credentials.client_id, credentials.client_secret);
+
+                    oa2.getOAuthAccessToken(
+                        credentials.refresh_token,
+                        {grant_type : 'refresh_token'},
+                        function(error, oauth_access_token, oauth_refresh_token, results){
+                            if (error) {
+                                node.status({fill:"red",shape:"dot",text:"Token Refresh error, please re-authenticate."});
+                                checkTokenExpiry();
+                            } else {
+                                credentials.access_token = oauth_access_token;
+                                credentials.refresh_token = oauth_refresh_token;
+
+                                RED.nodes.addCredentials(nodeid, credentials);
+
+                                token_valid = true;
+                            }
+                        }
+                    );
+                } else if (httpResponse.statusCode != 200) {
+                    node.status({fill:"red",shape:"dot",text:"Server error!! Are we maintaining the server?"});
+                } else {
+                    token_valid = true;
+                }
+            });
+        }
+
+        function connectWebsocket() {
+            var credentials = RED.nodes.getCredentials(node.user);
+            console.log(credentials.access_token);
+            var ws = new WebSocket('wss://futurehome.no/api/v2/sites/' + node.site + '/stream?access_token=' + credentials.access_token);
+
+            ws.on('open', function() {
+                node.status({fill:"green",shape:"dot",text:"Connected"});
+            });
+
+            ws.on('message',function(data) {
+                data = JSON.parse(data);
+                if (data.devices || data.rooms) {
+                    if (data.devices) {
+                        node.send({payload: {type: "device", params: data.devices[0]}});
+                    } else if (data.rooms) {
+                        node.send({payload: {type: "room", params: data.rooms[0]}});
+                    }
+                }
+            });
+        }
+
+        var configurationTimeout = setTimeout(checkForConfigurationStatus, 100);
     }
 
     RED.nodes.registerType("futurehome-credentials", FuturehomeOAuthNode, {
@@ -17,6 +111,13 @@ module.exports = function(RED) {
            client_id: {type:"password"},
            client_secret: {type: "password"}
        }
+    });
+
+    RED.nodes.registerType("site in", FuturehomeSiteNode, {
+        deafults: {
+            user: {type:"futurehome-credentials",required:true},
+            site: {value:"", required:true}
+        }
     });
 
     function getOAuth2(base_uri, client_id, client_secret) {
@@ -52,6 +153,13 @@ module.exports = function(RED) {
 
         var url = oa2.getAuthorizeUrl({redirect_uri : req.query.callback, response_type: "code", state: nodeid + ":" + csrfToken});
         res.redirect(url);
+    });
+
+    RED.httpAdmin.get('/futurehome/sites/:id', function(req, res, next) {
+        var nodeid = req.params.id;
+
+        var credentials = RED.nodes.getCredentials(nodeid);
+        res.send(credentials.sites);
     });
 
     RED.httpAdmin.get('/futurehome/authorize/callback', function(req, res, next) {
@@ -115,9 +223,4 @@ module.exports = function(RED) {
                      }
         );
     });
-
-    function FuturehomeSiteNode(config) {
-        RED.nodes.createNode(this,config);
-    }
-    RED.nodes.registerType("site", FuturehomeSiteNode);
 }
